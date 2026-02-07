b08f7a5d2920e61c5aedda1f3439c9e6f2092556
reynosom260-arch <reynosom260@gmail.com>
2026-02-06 19:14:15 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
-    SuperBitV2.Servo(SuperBitV2.enServo.S1, 2)
+    SuperBitV2.MotorRun(SuperBitV2.enMotors.M1, 5)
 })
 input.onButtonPressed(Button.B, function () {
-    SuperBitV2.Servo(SuperBitV2.enServo.S3, 4)
+    SuperBitV2.MotorRun(SuperBitV2.enMotors.M1, 10)
 })
